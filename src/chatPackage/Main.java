483979656d945ce3039a197b355684c392b1be8f
Mykyta <bri020697@gmail.com>
2016-11-04 23:35:06 +0200

    Rewrite code, now it's work better
    I still didn't realize a "many-clients-interface"
    (Shame on me..)

--- src/chatPackage/Main.java
+++ src/chatPackage/Main.java
@@ -1,14 +1,43 @@
 package chatPackage;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		chatServer startServerThread = new chatServer();
-		startServerThread.start();
-		chatClient startClientThread = new chatClient();
-		startClientThread.start();
+	public static void main(String[] args){
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		String choise;
+		System.out.println("write \"server\" to run in Server mode");
+		System.out.println("write \"client\" to run in Client mode");
+		
+		try{
+			chosen: 
+			while(true){
+				
+				choise = ((keyboard.readLine()).toLowerCase());
+				
+				switch(choise){
+				
+					case "server":	
+						chatServer startServerThread = new chatServer();
+						startServerThread.start();
+						break chosen;
+					case "client":
+						chatClient startClientThread = new chatClient();
+						startClientThread.start();
+						break chosen;
+					default:
+						break;
+				}
+				
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
